cf71ece719bd7c8e4a8dbdba1ed6e19ea6e72844
Audrey <appere.audrey29@gmail.com>
2023-06-24 23:16:22 +0200

    page about me

--- app/public/wp-content/themes/Portfolio/footer.php
+++ app/public/wp-content/themes/Portfolio/footer.php
@@ -4,6 +4,11 @@ get_template_part('templates-parts/content', 'modal');
 ?>
 <footer class="footer">
 
+    <!-- <div class="footer-img"> -->
+    <a href="<?php echo home_url('/'); ?>">
+        <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logoW.png'" alt="Logo">
+    </a>
+    <!-- </div> -->
 
     <?php wp_nav_menu([
         'theme_location' => 'footer',
--- app/public/wp-content/themes/Portfolio/header.php
+++ app/public/wp-content/themes/Portfolio/header.php
@@ -21,22 +21,22 @@
         ]);
         ?>
     </div>
-    <nav id="site-navigation" class="main-navigation navbar">
+    <!-- <nav id="site-navigation" class="main-navigation navbar">
         <div class="menu-mobile">
             <button class="menu-toggle close" aria-controls="primary-menu" aria-expanded="false">
                 <div class="line line_one"></div>
                 <div class="line line_two"></div>
                 <div class="line line_three"></div>
             </button>
-        </div>
-        <ul class="open_nav close_nav navbar-links">
-            <?php wp_nav_menu([
-                'theme_lacation' => 'main',
-                // 'container' => false,
-                'menu_class' => 'navbar-burger'
-            ]); ?>
-            <!-- <li class="menu-item nav-item"><a href="#" id="myBtn" class="modal-js" role="button" data-toggle="modal">CONTACT</a></li> -->
-        </ul>
+        </div> -->
+    <ul class="open_nav close_nav navbar-links">
+        <?php wp_nav_menu([
+            'theme_lacation' => 'main',
+            // 'container' => false,
+            'menu_class' => 'navbar-burger'
+        ]); ?>
+        <!-- <li class="menu-item nav-item"><a href="#" id="myBtn" class="modal-js" role="button" data-toggle="modal">CONTACT</a></li> -->
+    </ul>
 
 
 </header>
